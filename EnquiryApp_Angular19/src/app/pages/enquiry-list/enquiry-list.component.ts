84ef23b6e936a0655afaa843e17b195225716fc5
Aditya Yerudkar <127290435+adityayerudkar2311@users.noreply.github.com>
2025-09-15 22:26:05 +0530

    Initial commit

---
+++ EnquiryApp_Angular19/src/app/pages/enquiry-list/enquiry-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enquiry-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './enquiry-list.component.html',
+  styleUrl: './enquiry-list.component.css'
+})
+export class EnquiryListComponent {
+
+}
